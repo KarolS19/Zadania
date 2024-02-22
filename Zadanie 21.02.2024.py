c60d29a6b52d8c15feb21d3de2c4b807a14d632c
Karol Śpiewak <115652271+KarolS19@users.noreply.github.com>
2024-02-22 18:57:59 +0100

    Update Zadanie 21.02.2024.py

--- Zadanie 21.02.2024.py
+++ Zadanie 21.02.2024.py
@@ -1,14 +1,55 @@
-import json
+import pickle
 
-# Funkcja do zapisywania danych do pliku
-def zapisz_do_pliku(ksiazka_adresowa, nazwa_pliku):
-    with open(nazwa_pliku, 'w') as plik:
-        json.dump(ksiazka_adresowa, plik)
+class Contact:
+    def __init__(self, name, phone_number):
+        self.name = name
+        self.phone_number = phone_number
 
-# Funkcja do wczytywania danych z pliku
-def wczytaj_z_pliku(nazwa_pliku):
-    try:
-        with open(nazwa_pliku, 'r') as plik:
-            return json.load(plik)
-    except FileNotFoundError:
-        return {} 
+class AddressBook:
+    def __init__(self):
+        self.contacts = []
+
+    def add_contact(self, contact):
+        self.contacts.append(contact)
+
+    def save_address_book(self, filename):
+        with open(filename, 'wb') as file:
+            pickle.dump(self.contacts, file)
+
+    def load_address_book(self, filename):
+        with open(filename, 'rb') as file:
+            self.contacts = pickle.load(file)
+
+    def search_contacts(self, query):
+        results = []
+        for contact in self.contacts:
+            if query.lower() in contact.name.lower() or query in contact.phone_number:
+                results.append(contact)
+        return results
+
+def main():
+    address_book = AddressBook()
+
+    # Przykładowe dodanie kontaktów
+    address_book.add_contact(Contact("Karol Spiewak", "123456789"))
+    address_book.add_contact(Contact("Jan Kowalski", "987654321"))
+
+    # Zapisanie książki adresowej
+    address_book.save_address_book("address_book.pkl")
+
+    # Utworzenie nowej książki adresowej i wczytanie z pliku
+    new_address_book = AddressBook()
+    new_address_book.load_address_book("address_book.pkl")
+
+    # Wyszukiwanie kontaktów
+    query = input("Wyszukaj kontakty: ")
+    results = new_address_book.search_contacts(query)
+    if results:
+        print("Znalezione kontakty:")
+        for contact in results:
+            print(f"Imię: {contact.name}, Numer telefonu: {contact.phone_number}")
+    else:
+        print("Brak pasujących kontaktów.")
+
+if __name__ == "__main__":
+    main()
