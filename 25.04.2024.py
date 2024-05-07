47ffe9563365cc637d1ec758119280d71f20955b
Karol Śpiewak <115652271+KarolS19@users.noreply.github.com>
2024-05-07 21:09:26 +0200

    Update 25.04.2024.py

--- 25.04.2024.py
+++ 25.04.2024.py
@@ -4,40 +4,30 @@ import json
 from datetime import datetime, timedelta
 
 class NBPApiClient:
-    BASE_URL = "http://https://api.nbp.pl"
+    BASE_URL = "http://api.nbp.pl/api/exchangerates/rates/A/EUR,USD"
 
     async def fetch_exchange_rates(self, days):
-        exchange_rates = []
+        exchange_rates = {}
         async with aiohttp.ClientSession() as session:
-            for i in range(days):
-                date = datetime.now() - timedelta(days=i)
-                formatted_date = date.strftime("%Y-%m-%d")
-                url = f"{self.BASE_URL}{formatted_date}"
-                try:
-                    async with session.get(url) as response:
-                        data = await response.json()
-                        exchange_rate_data = self.extract_eur_usd_rate(data)
-                        exchange_rates.append({formatted_date: exchange_rate_data})
-                except Exception as e:
-                    print(f"Error fetching data for {formatted_date}: {e}")
+            try:
+                async with session.get(self.BASE_URL) as response:
+                    data = await response.json()
+                    rates_data = data['rates']
+                    for rate_data in rates_data:
+                        date = datetime.strptime(rate_data['effectiveDate'], "%Y-%m-%d")
+                        exchange_rates[date.strftime("%Y-%m-%d")] = {
+                            'EUR': {
+                                'sale': rate_data['mid'],
+                                'purchase': rate_data['mid']
+                            },
+                            'USD': {
+                                'sale': rate_data['mid'],
+                                'purchase': rate_data['mid']
+                            }
+                        }
+            except Exception as e:
+                print(f"Error fetching data: {e}")
         return exchange_rates
-
-    def extract_eur_usd_rate(self, data):
-        rates = data[0]['rates']
-        eur_data = next((rate for rate in rates if rate['code'] == 'EUR'), None)
-        usd_data = next((rate for rate in rates if rate['code'] == 'USD'), None)
-        if eur_data and usd_data:
-            return {
-                'EUR': {
-                    'sale': eur_data['mid'],
-                    'purchase': eur_data['mid']
-                },
-                'USD': {
-                    'sale': usd_data['mid'],
-                    'purchase': usd_data['mid']
-                }
-            }
-        return None
 
 async def main():
     days = 10  
